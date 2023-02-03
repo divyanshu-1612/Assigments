e4e8f048e071da7f7dec761764368ccefb7bee96
Divyanshu Gupta <divyanshucc1612@gmail.com>
2023-02-03 15:17:58 +0530

    Starting NodeJS

---
+++ NodeJS/Learning/execute.js
@@ -0,0 +1,10 @@
+// Child process
+
+const execute = require('child_process').exec;
+
+execute("ls", (err, stdout) => {
+    if(err){
+        return err;
+    }
+    console.log(stdout);
+});
---
+++ NodeJS/Learning/export.js
@@ -0,0 +1,4 @@
+const events = require('events');
+let emitter1 = new events.EventEmitter();
+
+module.exports = emitter1;
